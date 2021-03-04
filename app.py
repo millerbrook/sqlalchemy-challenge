8de5419946af14cf9d5b18c5c48673592c624570
Brook Miller <millerbrook@gmail.com>
2021-03-04 11:00:45 -0500

    Completes draft of Flask app

--- app.py
+++ app.py
@@ -41,6 +41,9 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precips():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     # Design a query to retrieve the last 12 months of precipitation data. 
     test = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
@@ -88,48 +91,100 @@ def stations():
     return jsonify(results)
 
 @app.route("/api/v1.0/tobs")
-    def tempObs():
-        #Rank stations by count in descending order
-        station_counts = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
-        #Choose first station
-        most_active_station = station_counts[0][0]
-        #Query dates and temps associated with this station
-        #Note it repeats lots of earlier query process--is this what is wanted?
-        # Design a query to retrieve the last 12 months of precipitation data. 
-        test = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+def tempObs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-        datetest = datetime.strptime(test[0], "%Y-%m-%d").date()
-        type(datetest)
-        # Starting from the most recent data point in the database. 
+    #Rank stations by count in descending order
+    station_counts = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    #Choose first station
+    most_active_station = station_counts[0][0]
+    #Query dates and temps associated with this station
+    #Note it repeats lots of earlier query process--is this what is wanted?
+    # Design a query to retrieve the last 12 months of precipitation data. 
+    test = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
-        # # Calculate the date one year from the last date in data set.
-        year_ago = datetest - dt.timedelta(days=365)
-        print(year_ago)
-        # Perform a query to retrieve the data and temperature scores
-        datestrings = session.query(Measurement.date).\
-            filter(Measurement.date > year_ago).\
-            filter(Measurement.station==most_active_station).\
-            order_by(Measurement.date).all()
+    datetest = datetime.strptime(test[0], "%Y-%m-%d").date()
+    type(datetest)
+    # Starting from the most recent data point in the database. 
 
-        dates = []
-        for onedate in datestrings:
-            convert = datetime.strptime(onedate[0], "%Y-%m-%d").date()
-            dates.append(convert)
+    # # Calculate the date one year from the last date in data set.
+    year_ago = datetest - dt.timedelta(days=365)
+    print(year_ago)
+    # Perform a query to retrieve the data and temperature scores
+    datestrings = session.query(Measurement.date).\
+        filter(Measurement.date > year_ago).\
+        filter(Measurement.station==most_active_station).\
+        order_by(Measurement.date).all()
 
-        temperatures = session.query(Measurement.tobs).\
-            filter(Measurement.date > year_ago).\
-            filter(Measurement.station == most_active_station).\
-            order_by(Measurement.date).all()
+    dates = []
+    for onedate in datestrings:
+        convert = datetime.strptime(onedate[0], "%Y-%m-%d").date()
+        dates.append(convert)
 
-        temp_list = []
-        for onetemp in temperatures:
-            temp_list.append(onetemp[0])
+    temperatures = session.query(Measurement.tobs).\
+        filter(Measurement.date > year_ago).\
+        filter(Measurement.station == most_active_station).\
+        order_by(Measurement.date).all()
 
-        activeStation_dict = {k:v for k,v in zip(dates,temp_list)}
+    temp_list = []
+    for onetemp in temperatures:
+        temp_list.append(onetemp[0])
 
-        session.close()
+    activeStation_dict = {k:v for k,v in zip(dates,temp_list)}
 
-        return jsonify(activeStation_dict)
+    #Close out the session
+    session.close()
+
+    return jsonify(activeStation_dict)
+    
+@app.route("/api/v1.0/<start>")
+def sinceStart(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    #Queries to calculate TMIN, TAVG, and TMAX since start date
+    #use func
+    try:
+        maxTemp = session.query(func.max(Measurement.tobs).filter(Measurement.date >= start).first()
+    except:
+        maxTemp = "Sorry I didn't recognize the date"
+    try:
+        minTemp = session.query(func.min(Measurement.tobs).filter(Measurement.date >= start).first()
+    except:
+        minTemp = ""
+    try:
+        avgTemp = session.query(func.avg(Measurement.tobs).filter(Measurement.date >= start).first()
+    except:
+        avgTemp = ""
         
+    #Close out the session
+    session.close()
+
+    return maxTemp, minTemp, avgTemp 
+
+@app.route("/api/v1.0/<start>/<end>")
+def startEnd(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    #Queries to calculate TMIN, TAVG, and TMAX since start date
+    #use func
+    try:
+        maxTemp = session.query(func.max(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+    except:
+        maxTemp = "Sorry I didn't recognize the dates"
+    try:
+        minTemp = session.query(func.min(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+    except:
+        minTemp = ""
+    try:
+        avgTemp = session.query(func.avg(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+    except:
+        avgTemp = ""
+
+    #Close out the session
+    session.close()
+
+    return maxTemp, minTemp, avgTemp 
+
 if __name__ == '__main__':
     app.run(debug=True)
