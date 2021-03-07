843906475307290347c57469238f81d912478154
Brook Miller <millerbrook@gmail.com>
2021-03-07 11:22:30 -0500

    Adds missing routes to root page

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/"
+
     )
 
 
